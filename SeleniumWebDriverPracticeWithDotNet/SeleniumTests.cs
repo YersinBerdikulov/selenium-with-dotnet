7b685ca0d3011a04c9370271ff720c548696a3c3
Yersin Berdikulov <221225@astanait.edu.kz>
2024-12-24 00:13:30 +0500

    Remove unnecessary using

--- SeleniumWebDriverPracticeWithDotNet/SeleniumTests.cs
+++ SeleniumWebDriverPracticeWithDotNet/SeleniumTests.cs
@@ -1,9 +1,84 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
 namespace SeleniumWebDriverPracticeWithDotNet;
 
-public class SeleniumTests
+public sealed class SeleniumTests : IDisposable
 {
-    [Fact]
-    public void UnitTest()
+    private readonly IWebDriver _webDriver;
+
+    public SeleniumTests()
     {
+        _webDriver = new FirefoxDriver();
+    }
+
+    [Fact]
+    public void SearchTest_ByUserImitation_UsingCssSelector()
+    {
+        //Arrange
+        _webDriver
+            .Navigate()
+            .GoToUrl("https://www.google.com");
+
+        var searchBar = _webDriver.FindElement(By.CssSelector("textarea#APjFqb"));
+
+        //Act
+        searchBar.SendKeys("Quality Assurance");
+        searchBar.SendKeys(Keys.Enter);
+
+        var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(3));
+        wait.Until(driver => driver.Title.Contains("Quality Assurance"));
+
+        //Assert
+        Assert.Contains("Quality Assurance", _webDriver.Title);
+    }
+
+    [Fact]
+    public void SearchTest_ByUserImitation_UsingXPath()
+    {
+        //Arrange
+        _webDriver
+            .Navigate()
+            .GoToUrl("https://www.google.com");
+
+        var searchBar = _webDriver.FindElement(By.XPath("//textarea[@id='APjFqb']"));
+
+        //Act
+        searchBar.SendKeys("Quality Assurance");
+        searchBar.SendKeys(Keys.Enter);
+
+        var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(3));
+        wait.Until(driver => driver.Title.Contains("Quality Assurance"));
+
+        //Assert
+        Assert.Contains("Quality Assurance", _webDriver.Title);
+    }
+
+    [Fact]
+    public void SearchTest_ByChangingUrlContents()
+    {
+        //Arrange
+        _webDriver
+            .Navigate()
+            .GoToUrl("https://www.google.com");
+
+        const string topicToSearch = "Quality+Assurance";
+
+        //Act
+        _webDriver.Url = _webDriver.Url + "/search?q=" + topicToSearch;
+
+        var wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(3));
+        wait.Until(driver => driver.Title.Contains("Quality Assurance"));
+
+        //Assert
+        Assert.Contains("Quality Assurance", _webDriver.Title);
+        Assert.Contains("Quality+Assurance", _webDriver.Url);
+    }
+
+    public void Dispose()
+    {
+        _webDriver.Quit();
+        _webDriver.Dispose();
     }
 }
