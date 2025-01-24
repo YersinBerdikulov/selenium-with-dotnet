c18674e52ba8f12e16d7eb5db458bb8221b58637
Yersin Berdikulov <221225@astanait.edu.kz>
2025-01-24 12:58:27 +0500

    Add log4net for logging

--- SeleniumWebDriverPracticeWithDotNet/BasicTests/SearchFunctionalityTests.cs
+++ SeleniumWebDriverPracticeWithDotNet/BasicTests/SearchFunctionalityTests.cs
@@ -1,9 +1,13 @@
 using System.Reflection;
 using AventStack.ExtentReports;
+using log4net;
+using log4net.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using Xunit.Abstractions;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
 
 namespace SeleniumWebDriverPracticeWithDotNet.BasicTests;
 
@@ -12,16 +16,20 @@ public sealed class SearchFunctionalityTests : IClassFixture<ExtentReportsFixtur
     private readonly ExtentReports _extent;
     private readonly ExtentTest _test;
     private readonly IWebDriver _webDriver;
+    private readonly ILog _logger;
 
     public SearchFunctionalityTests(ExtentReportsFixture extent, ITestOutputHelper output)
     {
         var type = output.GetType();
         var testMember = type.GetField("test", BindingFlags.Instance | BindingFlags.NonPublic);
         var test = (ITest)testMember?.GetValue(output)!;
+        var testMethodName = test.TestCase.TestMethod.Method.Name;
 
         _extent = extent.Extent;
         _test = _extent.CreateTest($"Search Functionality Tests - {test.TestCase.TestMethod.Method.Name}");
         _webDriver = new FirefoxDriver();
+        _logger = LogManager.GetLogger(typeof(SearchFunctionalityTests));
+        _logger.InfoFormat($"Running test case method - {testMethodName}");
     }
 
     [Fact]
@@ -29,14 +37,16 @@ public sealed class SearchFunctionalityTests : IClassFixture<ExtentReportsFixtur
     {
         try
         {
+            _logger.Info("Navigating to Google...");
             // Arrange
             _webDriver
                 .Navigate()
                 .GoToUrl("https://www.google.com");
-
+            
             var searchBar = _webDriver.FindElement(By.CssSelector("textarea#APjFqb"));
 
             // Act
+            _logger.Info("Performing search...");
             searchBar.SendKeys("Quality Assurance");
             searchBar.SendKeys(Keys.Enter);
 
@@ -44,11 +54,14 @@ public sealed class SearchFunctionalityTests : IClassFixture<ExtentReportsFixtur
             wait.Until(driver => driver.Title.Contains("Quality Assurance"));
 
             //Assert
+            _logger.Info("Validating test results...");
             Assert.Contains("Quality Assurance", _webDriver.Title);
             _test.Log(Status.Pass, $"Test assertion passed. Page title contains {_webDriver.Title}");
+            _logger.Info("Test passed");
         }
         catch (Exception e)
         {
+            _logger.Error("Test failed", e);
             _test.Log(Status.Fail, "Test Failed. See below for details.");
             _test.Log(Status.Error, e.Message);
         }
@@ -116,8 +129,11 @@ public sealed class SearchFunctionalityTests : IClassFixture<ExtentReportsFixtur
 
     public void Dispose()
     {
+        _logger.Info("Cleaning up resources");
+        _logger.Info("Closing web driver...");
         _webDriver.Quit();
         _webDriver.Dispose();
+        _logger.Info("Finalizing report");
         ExtentReportsHelper.FinalizeReport();
     }
 }
